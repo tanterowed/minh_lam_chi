54900f6ca6b0163db2682929648f3c74ebae39e0
tanterowed <tanterowed@github.com>
2024-08-16 23:22:38 +0700

    init

---
+++ s-u-n-d-a-y-w-e-b-r-y.meta.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         s-u-n-d-a-y-w-e-b-r-y
+// @namespace    https://github.com/tanterowed/minh_lam_chi/raw/main/s-u-n-d-a-y-w-e-b-r-y.user.js
+// @version      2024.08.16
+// @description  down s-u-n-d-a-y-w-e-b-r-y
+// @author       You
+// @include      /^https:\/\/www\.sund[a@]y-w[e3]bry\.c[o0]m\/ep[i1]s[o0]de\//
+// @icon         https://raw.githubusercontent.com/tanterowed/minh_lam_chi/main/s-u-n-d-a-y-w-e-b-r-y.png
+// @grant        GM_download
+// @grant        GM_info
+// @downloadURL  https://github.com/tanterowed/minh_lam_chi/raw/main/s-u-n-d-a-y-w-e-b-r-y.user.js
+// @updateURL    https://github.com/tanterowed/minh_lam_chi/raw/main/s-u-n-d-a-y-w-e-b-r-y.meta.js
+// ==/UserScript==
